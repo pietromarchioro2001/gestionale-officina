b31fc61c23d9710cf529b96023efa30143068b4a
pietromarchioro2001 <pietromarchioro2001@gmail.com>
2026-02-02 17:41:34 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const BACKEND_URL = "https://script.google.com/macros/s/AKfycbxd3waQJf7MbKCCzQaSvjuV-3vHDcLI_PEdk-zCDr2YnNV11exMB39CSRAbXtlMfokj/exec";
+const BACKEND_URL = "https://script.google.com/macros/s/AKfycbyQkoinsAke8ffIxpd-UAVUTpPBVa6GfOKLFagt9AILxYo8OzMPs8m7o4J2CGSBtuo1/exec";
 
 function testBackend() {
   fetch(BACKEND_URL)
@@ -15,4 +15,18 @@ function testBackend() {
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker.register("service-worker.js");
 }
+
+function caricaClienti() {
+  const s = document.createElement("script");
+
+  window.cbClienti = data => {
+    document.getElementById("output").textContent =
+      JSON.stringify(data, null, 2);
+    s.remove();
+  };
+
+  s.src = BACKEND_URL + "?action=clienti&callback=cbClienti";
+  document.body.appendChild(s);
+}
+
 
