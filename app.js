265dd6b37d567025b3905fbb1ec18f4397906c9d
pietromarchioro2001 <pietromarchioro2001@gmail.com>
2026-02-02 17:58:55 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
 const BACKEND_URL = "https://script.google.com/macros/s/AKfycbyQkoinsAke8ffIxpd-UAVUTpPBVa6GfOKLFagt9AILxYo8OzMPs8m7o4J2CGSBtuo1/exec";
 
+// test semplice
 function testBackend() {
-  fetch(BACKEND_URL)
-    .then(r => r.text())
-    .then(t => {
-      document.getElementById("output").textContent = t;
-    })
-    .catch(e => {
-      document.getElementById("output").textContent = e;
-    });
-}
-
-// PWA
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("service-worker.js");
-}
-
-function caricaClienti() {
   const s = document.createElement("script");
 
-  window.cbClienti = data => {
+  window.testCallback = function (data) {
     document.getElementById("output").textContent =
       JSON.stringify(data, null, 2);
     s.remove();
   };
 
-  s.src = BACKEND_URL + "?action=clienti&callback=cbClienti";
+  s.src = BACKEND_URL + "?callback=testCallback";
   document.body.appendChild(s);
 }
+
+// clienti
+function caricaClienti() {
+  const s = document.createElement("script");
+
+  window.clientiCallback = function (data) {
+    document.getElementById("output").textContent =
+      JSON.stringify(data, null, 2);
+    s.remove();
+  };
+
+  s.src = BACKEND_URL + "?action=clienti&callback=clientiCallback";
+  document.body.appendChild(s);
+}
+
 
 
