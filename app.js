1e7ccd4170a6ae1dc24ad9a6e5b16965b9a7c82f
pietromarchioro2001 <pietromarchioro2001@gmail.com>
2026-02-04 18:32:54 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbwEPdYK7GTPybozEbkQrvDVOiNMOyP1qdi03I0WpNG1bRw0IXdfsZx-f3NgIFmpf80J/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbz_Q9KJQUKesamDT_3YtT7-5tE-fjPcQGwEmvYvA1exsU-KY62YpvSQm-UqK_L_F2Jb/exec";
 
 function callBackend(action, args = []) {
 
@@ -2155,6 +2155,7 @@ document.addEventListener("DOMContentLoaded", () => {
   resetFileInput("altriDocumenti", "altriLink");
 
 });
+
 
 
 
