2216aa3659b934f58f7b03daa7766965b70d282a
pietromarchioro2001 <pietromarchioro2001@gmail.com>
2026-02-16 15:01:00 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbz8hVdTAWsL_PVpMJWVOfVJke5I1VKOZ0v2uaxd0bJSu8ZZVTitdfExJn26nYbDAQnJ/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbykOf2ObqSdDjg9IQq4He4L5ZxYNluALxxz8aA1sYKbf4YYuQcgRA9J8tZlsifaxPI-/exec";
 
 let TEMP_LIBRETTO_ID = null;
 let TEMP_TARGA_ID = null;
@@ -2198,6 +2198,7 @@ document.addEventListener("DOMContentLoaded", () => {
   resetFileInput("altriDocumenti", "altriLink");
 
 });
+
 
 
 
