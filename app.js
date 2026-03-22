ffcfb028656b9261f97bd0b43d16b53dee2b5674
pietromarchioro2001 <pietromarchioro2001@gmail.com>
2026-03-22 21:50:14 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbzGBowQnnPk6FhZ_Plo3NNi6fjk79Bmu_k6iAUHwIgYCPjw_4miGL9wCw7WFoBTiARf/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbxQ0AosVZOcbE5WJm4wLkRX3zOv9SCf7DSpVyPdW0kIO2hwQZZVLZn5pdeXBP--06jl/exec";
 
 const ICON_CALENDAR = `
 <svg viewBox="0 0 24 24">
