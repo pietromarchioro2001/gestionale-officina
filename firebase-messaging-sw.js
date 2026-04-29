868aa7105a4cfbd7d777a20b8ea90a3a55fd1379
pietromarchioro2001 <pietromarchioro2001@gmail.com>
2026-04-29 10:51:50 +0200

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,12 +2,12 @@ importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js
 importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js');
 
 firebase.initializeApp({
-  apiKey: "QUI_LA_TUA_API_KEY",
-  authDomain: "QUI",
-  projectId: "QUI",
-  storageBucket: "QUI",
-  messagingSenderId: "QUI",
-  appId: "QUI"
+  apiKey: "AIzaSyC4OPs05sPxSb5H6LQSwV9b4-bMrGfMYjY",
+  authDomain: "goldencar-notifiche.firebaseapp.com",
+  projectId: "goldencar-notifiche",
+  storageBucket: "goldencar-notifiche.firebasestorage.app",
+  messagingSenderId: "932662604015",
+  appId: "1:932662604015:web:2d3a38bcbdd9c12253ab1a"
 });
 
 const messaging = firebase.messaging();
